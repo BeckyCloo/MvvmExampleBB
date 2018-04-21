ae78395535f4df26a83e8d239e15a25f7368768c
darklila <darklila@web.de>
2018-04-21 12:28:53 +0200

    Add project files.

---
+++ MvvmExampleBB/Model/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmExampleBB.Model
+{
+    public class Song
+    {
+        private string _artistName;
+        private string _songTitle;
+
+        public string ArtistName { get => this._artistName; set => this._artistName = value; }
+
+        //public string ArtistName
+        //{
+        //    get { return _artistName; }
+        //    set { _artistName = value; }
+        //}
+
+        public string SongTitle { get => this._songTitle; set => this._songTitle = value; }
+
+        //public string GetSongTitle()
+        //{
+        //    return this._songTitle;
+        //}
+        //public void SetSongTitle(string value)
+        //{
+        //    this._songTitle = value;
+        //}
+    }
+}
---
+++ MvvmExampleBB/ViewModel/SongViewModel.cs
@@ -0,0 +1,53 @@
+﻿using MvvmExampleBB.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmExampleBB.ViewModel
+{
+    public class SongViewModel : INotifyPropertyChanged
+    {
+        #region Construction
+        /// <summary>
+        /// Constructs the default instance of a SongViewModel
+        /// </summary>
+        public SongViewModel()
+        {
+            _song = new Song { ArtistName = "Unknown", SongTitle = "Unknown" };
+        }
+        #endregion
+
+        private Song _song;
+
+        public Song Song { get => this._song; set => this._song = value; }
+
+        public string ArtistName
+        {
+            get => Song.ArtistName;
+            set
+            {
+                if (Song.ArtistName != value)
+                {
+                    Song.ArtistName = value;
+                    RaisePropertyChanged("ArtistName");
+
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            //take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
